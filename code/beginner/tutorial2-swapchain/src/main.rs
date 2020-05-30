3e4898bcd7bc321d362572cf686702255fb69541
Joshua Groves <josh@joshgroves.com>
2020-05-30 20:40:13 -0230

    Change example slightly

--- code/beginner/tutorial2-swapchain/src/main.rs
+++ code/beginner/tutorial2-swapchain/src/main.rs
@@ -116,45 +116,37 @@ fn main() {
     let mut state = block_on(State::new(&window));
 
     event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
         match event {
             Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == window.id() => if !state.input(event) {
-                match event {
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    WindowEvent::KeyboardInput {
-                        input,
-                        ..
-                    } => {
-                        match input {
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            } => *control_flow = ControlFlow::Exit,
-                            _ => {}
-                        }
-                    }
-                    WindowEvent::Resized(physical_size) => {
-                        state.resize(*physical_size);
-                    }
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        // new_inner_size is &mut so w have to dereference it twice
-                        state.resize(**new_inner_size);
-                    }
-                    _ => {}
-                }
-            }
+                event: WindowEvent::Resized(size),
+                ..
+            } => state.resize(size),
             Event::RedrawRequested(_) => {
-                state.update();
-                state.render();
+                let frame = state.swap_chain
+                    .get_next_texture()
+                    .expect("Timeout when acquiring next swap chain texture");
+                let mut encoder =
+                    state.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+                {
+                    encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                            attachment: &frame.view,
+                            resolve_target: None,
+                            load_op: wgpu::LoadOp::Clear,
+                            store_op: wgpu::StoreOp::Store,
+                            clear_color: wgpu::Color::GREEN,
+                        }],
+                        depth_stencil_attachment: None,
+                    });
+                }
+
+                state.queue.submit(&[encoder.finish()]);
             }
-            Event::MainEventsCleared => {
-                // RedrawRequested will only trigger once, unless we manually
-                // request it.
-                window.request_redraw();
-            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
             _ => {}
         }
     });
